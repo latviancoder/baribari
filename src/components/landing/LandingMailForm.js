d7c70651acccfedc011ee3b897e6c867c93f82dc
latviancoder <igvadaimon@gmail.com>
2015-11-15 11:31:46 +0100

    debug

--- src/components/landing/LandingMailForm.js
+++ src/components/landing/LandingMailForm.js
@@ -4,13 +4,12 @@ var { FormattedMessage, injectIntl } = require('react-intl');
 
 var LandingMailForm = React.createClass({
 	render() {
-		var { intl } = this.props;
+		//var { intl } = this.props;
 
 		return <form onSubmit={this.__onMailFormSubmitted}>
 
 			<input ref="mailInput"
-			       className="landing-mail-input"
-			       placeholder={intl.messages['mail.placeholder']}/>
+			       className="landing-mail-input"/>
 
 			<button type="submit" className="landing-mail-submit">
 				<FormattedMessage id="mail.submit"/>
@@ -21,12 +20,12 @@ var LandingMailForm = React.createClass({
 	__onMailFormSubmitted(e) {
 		alert('111');
 		e.preventDefault();
-		alert('222');
-		alert(e);
-		alert(this.refs.mailInput);
-		alert(this.refs.mailInput.value);
-		this.props._addMail(this.refs.mailInput.value);
+		//alert('222');
+		//alert(e);
+		//alert(this.refs.mailInput);
+		//alert(this.refs.mailInput.value);
+		//this.props._addMail(this.refs.mailInput.value);
 	}
 });
 
-module.exports = injectIntl(LandingMailForm);
+module.exports = LandingMailForm;
