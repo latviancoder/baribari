e67570f08842f165af7086962f7e0c0a5dd6fdad
latviancoder <igvadaimon@gmail.com>
2015-11-15 11:45:31 +0100

    remove debug

--- src/components/landing/LandingMail.js
+++ src/components/landing/LandingMail.js
@@ -31,16 +31,12 @@ var LandingMail = React.createClass({
 	},
 
 	_addMail(email) {
-		alert('parent');
 		var locale = LocaleStore.getState();
 
-		alert(locale)
 		request
 			.post('/api/test')
 			.send({email, locale})
 			.end();
-
-		alert('request submitted')
 
 		this.setState({submitted: true});
 	}
--- src/components/landing/LandingMailForm.js
+++ src/components/landing/LandingMailForm.js
@@ -4,12 +4,15 @@ var { FormattedMessage, injectIntl } = require('react-intl');
 
 var LandingMailForm = React.createClass({
 	render() {
-		//var { intl } = this.props;
+		var { intl } = this.props;
 
 		return <form onSubmit={this.__onMailFormSubmitted}>
 
-			<input ref="mailInput"
-			       className="landing-mail-input"/>
+			<input type="email"
+			       required="required"
+			       ref="mailInput"
+			       className="landing-mail-input"
+			       placeholder={intl.messages['mail.placeholder']}/>
 
 			<button type="submit" className="landing-mail-submit">
 				<FormattedMessage id="mail.submit"/>
@@ -18,14 +21,10 @@ var LandingMailForm = React.createClass({
 	},
 
 	__onMailFormSubmitted(e) {
-		alert('111');
 		e.preventDefault();
-		//alert('222');
-		//alert(e);
-		//alert(this.refs.mailInput);
-		//alert(this.refs.mailInput.value);
-		//this.props._addMail(this.refs.mailInput.value);
+
+		this.props._addMail(this.refs.mailInput.value);
 	}
 });
 
-module.exports = LandingMailForm;
+module.exports = injectIntl(LandingMailForm);
